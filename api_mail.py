2eecc28fc27b6e3c315dbf445d588e326d765458
syltr1x <syltr1x@proton.me>
2024-06-01 02:12:51 -0300

    fix: status code error, delete useless lines

--- api_mail.py
+++ api_mail.py
@@ -11,14 +11,11 @@ def add_account(email, password):
     }
     headers = { 'Content-Type': 'application/json' }
     r = requests.post(url, headers=headers, json=payload)
-    data = r.text
-    data = json.loads(data)
-    r = str(r)
-    print("------------------------------")
-    if "201" in r: print(c.GREEN+"[+] Acount Created"+c.WHITE)
+    data = json.loads(r.text)
+    if 201 == r.status_code: print(c.GREEN+"[+] Acount Created"+c.WHITE)
     else: 
-        if "400" in r: print(c.RED+"[-] Err: Invalid values"+c.WHITE)
-        elif "422" in r: print(c.RED+"[-] Err: Account existent"+c.WHITE)
+        if 400 == r.status_code: print(c.RED+"[-] Err: Invalid values"+c.WHITE)
+        elif 422 == r.status_code: print(c.RED+"[-] Err: Account existent"+c.WHITE)
         else: print(c.RED+"[-] Err: Unknow. Code Err : "+r+c.WHITE)
 
 def get_token(email, password):
@@ -29,20 +26,16 @@ def get_token(email, password):
     }
     headers = { 'Content-Type': 'application/json' }
     r = requests.post(url, headers=headers, json=payload)
-    data = r.text
-    data = json.loads(data)
-    r = str(r)
-    if "200" in r.text: return data['token']
+    data = json.loads(r.text)
+    if 200 in r.status_code: return data['token']
     else: print(c.RED+"[-] Err : Unknow. Code Err : "+r+c.WHITE) 
 
 def delete_account(id, token):
     header = {"authorization":f"Bearer {token}"}
     r = requests.delete(f"https://api.mail.gw/accounts/{id}", headers=header)
-    r = str(r)
-    if "204" in r: print(c.GREEN+"[+] Account Deleted"+c.WHITE)
-    else: 
-        if "404" in r: print(c.RED+"[-] Err : Cuenta inexistente."+c.WHITE)
-        else: print(c.RED+"[-] Err : Unknow. Code Err : "+r+c.WHITE)
+    if r.status_code == 204 : print(c.GREEN+"[+] Account Deleted"+c.WHITE)
+    elif r.status_code == 404: print(c.RED+"[-] Err : Cuenta inexistente."+c.WHITE)
+    else: print(c.RED+"[-] Err : Unknow code Err : "+r+c.WHITE)
 
 def show_msg(token):
     header = {"authorization":f"Bearer {token}"}
@@ -52,14 +45,10 @@ def show_msg(token):
     if str(mail_sayisi["hydra:member"]) != "[]":
         id = mail_sayisi["hydra:member"][0]["id"]
         r  = requests.get(f"https://api.mail.gw/messages/{id}", headers=header)
-        mail = r.text
-        mail = json.loads(mail)
+        mail = json.loads(r.text)
         
         mailS = mail["from"]["address"]
         title = mail["subject"]
         content = mail["text"]
 
-        print("\n------------------------------")
-        print(c.GREEN+" Remitente / From : "+c.WHITE+mailS+c.GREEN+"\n Asunto / Subject : "+c.WHITE+title+c.GREEN+"\n Contenido / Content : "+c.WHITE+content)
-    else:
-        print(c.RED+"[!] NO TIENES NINGUN MENSAJE"+c.WHITE)
+        return mail
